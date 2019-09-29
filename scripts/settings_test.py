a4f086ecb5f3e3b1855e93e65ebd5720e45826fd
Moises9 <moises.estat@gmail.com>
2019-09-29 19:54:16 -0300

    load test

---
+++ scripts/settings_test.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 29 17:24:29 2019
+
+@author: moises
+"""
+
+"Bibliotecas instaladas "
+
+import pandas as pd
+
+import matplotlib as mpl
+
+# "pd.test
+
+a = pd.DataFrame({ "a" : [2, 12, 12, 3, 1],
+                    "B" : [ 1, 2,1, 2, 1]} )
+
+print(a)
